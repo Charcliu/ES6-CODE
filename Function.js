94227b48c4a03e009ea9e8bb2b8d5935b79b20bb
Wofluently <f_liuchang@163.com>
2019-01-22 16:15:04 +0800

    ES6 函数扩展

---
+++ Function.js
@@ -0,0 +1,30 @@
+function log(x, y) {
+  y = y || 'World'
+  console.log(x, y)
+}
+
+console.log(log('Hello'))
+console.log(log('Hello', 'ChangLau'))
+
+function defaultLog(x, y = 'World') {
+  console.log(x, y)
+}
+
+console.log(defaultLog('Hello'))
+console.log(defaultLog('Hello', 'ChangLau'))
+
+function point(x = 0, y = 0) {
+  this.x = x
+  this.y = y
+}
+
+console.log(new point())
+
+let x = 99
+function foo(p = x + 1) {
+  console.log(p)
+}
+console.log(foo()) // 100
+x = 100
+console.log(foo()) // 101
+console.log(foo(123)) // 123
